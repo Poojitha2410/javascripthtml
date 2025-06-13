77664387ccba53dcc66f837978a83d9cfd5387ca
d.poojitha <doosanapudipoojitha@gmail.com>
2025-06-13 10:32:06 +0530

    Create apppoojitha.js

---
+++ apppoojitha.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Poojitha from './poojitha';
+
+function App() {
+  return (
+    
+      <Poojitha >
+    
+      </Poojitha>
+       
+    
+  );
+}
+
+export default App;
