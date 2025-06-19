bb521756698e1b8b8d84b8afc8bfa6a73d9db94c
d.poojitha <doosanapudipoojitha@gmail.com>
2025-06-19 11:30:45 +0530

    Add files via upload

---
+++ operators/Arithmetic.java
@@ -0,0 +1,19 @@
+public class Arithmetic
+ {
+    public static void main(String[] args) 
+{
+        int a = 10;  
+        int b = 5;   
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));  
+    }
+}
+
+
+
